d85c300a5e5e6e14551c42ba06d425e4bc730b95
Karcadian <jimmyle@iastate.edu>
2024-08-23 22:47:10 -0500

    Updated site name.

--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -10,6 +10,7 @@ const Search = ({ onSearchChange }) => {
       `${GEO_API_URL}/cities?minPopulation=50000&namePrefix=${inputValue}`,
       geoApiOptions
     )
+<<<<<<< HEAD
       .then(async (response) => {
         if (!response.ok) {
           console.error(`Failed to fetch data. Status: ${response.status}`);
@@ -38,16 +39,44 @@ const Search = ({ onSearchChange }) => {
           console.error("Error parsing JSON response:", jsonError);
           return { options: [] };
         }
+=======
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!data || !data.data || !Array.isArray(data.data)) {
+          throw new Error("Invalid response format from the API");
+        }
+  
+        const options = data.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name} ${city.countryCode}`,
+        }));
+  
+        return { options };
+>>>>>>> dad5fab5 (Initial commit)
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
         throw error;
       });
+<<<<<<< HEAD
   };  
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
+=======
+  };
+  
+
+  const handleOnChange = (searchData) => {
+      setSearch(searchData);
+      onSearchChange(searchData);
+>>>>>>> dad5fab5 (Initial commit)
   };
 
   return (
